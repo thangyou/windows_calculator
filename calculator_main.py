d7aa625bf4cdd96e254f2f8f40d42128211fdee7
thangyou <202030491@pukyong.ac.kr>
2022-11-29 13:42:20 +0900

    resovles #4 - 수식/계산 결과 표시 항목 통합

--- calculator_main.py
+++ calculator_main.py
@@ -18,14 +18,15 @@ class Main(QDialog):
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Solution: ")
-        self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
+        layout_equation_solution = QGridLayout()
+
+        self.solution = QLabel()
+        self.equation = QLineEdit("0")
+        #self.equation.textChanged.connect(self.solution.setText)
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addRow(label_equation, self.equation)
-        layout_equation_solution.addRow(label_solution, self.solution)
+        layout_equation_solution.addWidget(self.solution)
+        layout_equation_solution.addWidget(self.equation)
 
         ### 연산 버튼
         button_plus = QPushButton("+")
