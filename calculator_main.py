5f1dc05bbc62b29bdf4e87971c35c27c3363ce6b
thangyou <202030491@pukyong.ac.kr>
2022-12-01 12:01:31 +0900

    [CSS] 계산기 크기 지정 및 QPushButton 색상 변경

--- calculator_main.py
+++ calculator_main.py
@@ -1,14 +1,54 @@
 import sys
 import math
 import numpy as np # atom numpy 설치 방법 확인 -> 실행 확인? ✓
+from PyQt5 import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+
+class QPushButtonOperation(QPushButton):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        font = QFont("Helvetia", 13)
+        font.setBold(True)
+        self.setFont(font)
+
+class QPushButtonNumber(QPushButton):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        font = QFont("Helvetia", 13)
+        font.setBold(True)
+        self.setFont(font)
+
+class QPushButtonEqual(QPushButton):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        font = QFont("Helvetia", 13)
+        font.setBold(True)
+        self.setFont(font)
+
+class QLineEdit(QLineEdit):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        font = QFont("Helvetia", 13)
+        self.setFont(font)
 
 class Main(QDialog):
     def __init__(self):
         super().__init__()
         self.title = "계산기"
         self.setWindowTitle(self.title)
+        self.setGeometry(700, 350, 400, 400)
+        self.set_style()
         self.init_ui()
+
+    def set_style(self):
+        with open("style_btn.css", 'r') as f:
+            self.setStyleSheet(f.read())
 
     def init_ui(self):
         ### 레이아웃 설정
@@ -29,10 +69,10 @@ class Main(QDialog):
         layout_equation_solution.addWidget(self.equation)
 
         ### 연산 버튼
-        button_plus = QPushButton("+")
-        button_minus = QPushButton("-")
-        button_product = QPushButton("x")
-        button_equal = QPushButton("=")
+        button_plus = QPushButtonOperation("+")
+        button_minus = QPushButtonOperation("-")
+        button_product = QPushButtonOperation("x")
+        button_equal = QPushButtonEqual("=")
 
         ### 연산 버튼을 클릭했을 때, 각 부호가 수식창에 추가될 수 있도록 시그널 설정
         button_plus.clicked.connect(lambda state, operation = "+": self.button_operation_clicked(operation))
@@ -47,15 +87,15 @@ class Main(QDialog):
         layout_operation2.addWidget(button_equal, 3, 0)
 
         ### 기존 계산기에 없는 항목(버튼)들 추가
-        button_rest = QPushButton("%")
-        button_clear = QPushButton("C") # Clear All
-        button_c_entry = QPushButton("CE") # Clear last char
-        button_backspace = QPushButton("Backspace")
+        button_rest = QPushButtonOperation("%")
+        button_clear = QPushButtonOperation("C") # Clear All
+        button_c_entry = QPushButtonOperation("CE") # Clear last char
+        button_backspace = QPushButtonOperation("<")
 
-        button_inverse = QPushButton("1/x") # 역수
-        button_square = QPushButton("x²") # 제곱
-        button_squ_root = QPushButton("²√x") # 제곱근
-        button_division = QPushButton("/")
+        button_inverse = QPushButtonOperation("1/x") # 역수
+        button_square = QPushButtonOperation("x²") # 제곱
+        button_squ_root = QPushButtonOperation("²√x") # 제곱근
+        button_division = QPushButtonOperation("/")
 
         ### 버튼 클릭 시 시그널 설정
         button_rest.clicked.connect(lambda state, operation = "%": self.button_operation_clicked(operation))
@@ -85,7 +125,7 @@ class Main(QDialog):
         ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
         number_button_dict = {}
         for number in range(0, 10):
-            number_button_dict[number] = QPushButton(str(number))
+            number_button_dict[number] = QPushButtonNumber(str(number))
             number_button_dict[number].clicked.connect(lambda state, num = number:
                                                        self.number_button_clicked(num))
             if number > 0:
@@ -95,11 +135,11 @@ class Main(QDialog):
                 layout_number.addWidget(number_button_dict[number], 3, 1)
 
         ### reverse 버튼과 소숫점 버튼을 입력하고 시그널 설정
-        button_reverse = QPushButton("+/-")
+        button_reverse = QPushButtonNumber("+/-")
         button_reverse.clicked.connect(lambda state, num = "-1": self.button_reverse_clicked(num))
         layout_number.addWidget(button_reverse, 3, 0)
 
-        button_dot = QPushButton(".")
+        button_dot = QPushButtonNumber(".")
         button_dot.clicked.connect(lambda state, num = ".": self.number_button_clicked(num))
         layout_number.addWidget(button_dot, 3, 2)
 
@@ -109,6 +149,7 @@ class Main(QDialog):
 
         self.setLayout(main_layout)
         self.show()
+
 
     #################
     ### functions ###
